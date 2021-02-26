f263c948c6e06f3aee03771cf33f4394b86269f1
HuanZ <huanzhangrain@gmail.com>
2021-02-26 22:27:39 +0800

    072

---
+++ problems/Java/String/dot_replaceAll_bracket_regex/Fisher_1108_Defanging_an_IP_Address.java
@@ -0,0 +1,19 @@
+
+//https://www.youtube.com/watch?v=FP0Na-pL0qk
+//LeetCode 1108: Defanging an IP Address - Interview Prep Ep 16
+//https://leetcode.com/problems/defanging-an-ip-address/
+
+class Solution {
+    public String defangIPaddr(String address) {
+        char[] t = 
+        for(int i=0)
+        
+    }
+}
+
+/*
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+*/
